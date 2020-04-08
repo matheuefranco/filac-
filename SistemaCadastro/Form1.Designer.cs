0eb98eda435ecdaae7659a46cd5b68e6f81b93e5
matheuefranco <matheusefranco@gmail.com>
2020-04-08 11:26:14 -0300

    InicioAula

---
+++ SistemaCadastro/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace SistemaCadastro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnCarregar = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblAtendimento = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabBuscar = new System.Windows.Forms.TabPage();
+            this.txtFila = new System.Windows.Forms.TextBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.tabCadastrar = new System.Windows.Forms.TabPage();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.txtCPF = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnAtender = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabBuscar.SuspendLayout();
+            this.tabCadastrar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Controls.Add(this.btnSalvar);
+            this.panel1.Controls.Add(this.btnCarregar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 597);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.FlatAppearance.BorderSize = 0;
+            this.btnSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalvar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.ForeColor = System.Drawing.Color.White;
+            this.btnSalvar.Location = new System.Drawing.Point(3, 115);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(194, 54);
+            this.btnSalvar.TabIndex = 9;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnCarregar
+            // 
+            this.btnCarregar.FlatAppearance.BorderSize = 0;
+            this.btnCarregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCarregar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCarregar.ForeColor = System.Drawing.Color.White;
+            this.btnCarregar.Location = new System.Drawing.Point(0, 55);
+            this.btnCarregar.Name = "btnCarregar";
+            this.btnCarregar.Size = new System.Drawing.Size(197, 54);
+            this.btnCarregar.TabIndex = 8;
+            this.btnCarregar.Text = "Carregar";
+            this.btnCarregar.UseVisualStyleBackColor = true;
+            this.btnCarregar.Click += new System.EventHandler(this.btnCarregar_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkRed;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(200, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1029, 30);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.DarkRed;
+            this.panel3.Controls.Add(this.lblAtendimento);
+            this.panel3.Location = new System.Drawing.Point(511, 29);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(388, 173);
+            this.panel3.TabIndex = 2;
+            // 
+            // lblAtendimento
+            // 
+            this.lblAtendimento.AutoSize = true;
+            this.lblAtendimento.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAtendimento.ForeColor = System.Drawing.Color.White;
+            this.lblAtendimento.Location = new System.Drawing.Point(110, 7);
+            this.lblAtendimento.Name = "lblAtendimento";
+            this.lblAtendimento.Size = new System.Drawing.Size(154, 42);
+            this.lblAtendimento.TabIndex = 4;
+            this.lblAtendimento.Text = "Próximo";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabBuscar);
+            this.tabControl1.Controls.Add(this.tabCadastrar);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tabControl1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(200, 264);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1029, 333);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabBuscar
+            // 
+            this.tabBuscar.Controls.Add(this.txtFila);
+            this.tabBuscar.Controls.Add(this.btnRemove);
+            this.tabBuscar.Controls.Add(this.btnBuscar);
+            this.tabBuscar.Controls.Add(this.label4);
+            this.tabBuscar.Controls.Add(this.txtBuscar);
+            this.tabBuscar.Location = new System.Drawing.Point(4, 32);
+            this.tabBuscar.Name = "tabBuscar";
+            this.tabBuscar.Padding = new System.Windows.Forms.Padding(3);
+            this.tabBuscar.Size = new System.Drawing.Size(1021, 297);
+            this.tabBuscar.TabIndex = 0;
+            this.tabBuscar.Text = "Buscar";
+            this.tabBuscar.UseVisualStyleBackColor = true;
+            // 
+            // txtFila
+            // 
+            this.txtFila.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFila.Location = new System.Drawing.Point(172, 55);
+            this.txtFila.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtFila.Multiline = true;
+            this.txtFila.Name = "txtFila";
+            this.txtFila.Size = new System.Drawing.Size(591, 227);
+            this.txtFila.TabIndex = 10;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRemove.Location = new System.Drawing.Point(834, 93);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(150, 54);
+            this.btnRemove.TabIndex = 9;
+            this.btnRemove.Text = "Remover";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscar.Location = new System.Drawing.Point(834, 18);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(150, 54);
+            this.btnBuscar.TabIndex = 7;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(67, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CPF";
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(172, 18);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(591, 32);
+            this.txtBuscar.TabIndex = 2;
+            // 
+            // tabCadastrar
+            // 
+            this.tabCadastrar.Controls.Add(this.btnConfirmar);
+            this.tabCadastrar.Controls.Add(this.txtCPF);
+            this.tabCadastrar.Controls.Add(this.label2);
+            this.tabCadastrar.Controls.Add(this.txtNome);
+            this.tabCadastrar.Controls.Add(this.label1);
+            this.tabCadastrar.Location = new System.Drawing.Point(4, 32);
+            this.tabCadastrar.Name = "tabCadastrar";
+            this.tabCadastrar.Padding = new System.Windows.Forms.Padding(3);
+            this.tabCadastrar.Size = new System.Drawing.Size(1021, 297);
+            this.tabCadastrar.TabIndex = 1;
+            this.tabCadastrar.Text = "Cadastrar";
+            this.tabCadastrar.UseVisualStyleBackColor = true;
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConfirmar.Location = new System.Drawing.Point(360, 155);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(228, 54);
+            this.btnConfirmar.TabIndex = 6;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Location = new System.Drawing.Point(156, 83);
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(740, 32);
+            this.txtCPF.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CPF";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(156, 28);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(740, 32);
+            this.txtNome.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.FlatAppearance.BorderSize = 0;
+            this.btnFechar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFechar.Image = global::SistemaCadastro.Properties.Resources.close;
+            this.btnFechar.Location = new System.Drawing.Point(1129, 36);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(88, 54);
+            this.btnFechar.TabIndex = 7;
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // btnAtender
+            // 
+            this.btnAtender.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAtender.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAtender.Location = new System.Drawing.Point(613, 214);
+            this.btnAtender.Name = "btnAtender";
+            this.btnAtender.Size = new System.Drawing.Size(175, 66);
+            this.btnAtender.TabIndex = 11;
+            this.btnAtender.Text = "Atender";
+            this.btnAtender.UseVisualStyleBackColor = true;
+            this.btnAtender.Click += new System.EventHandler(this.btnAtender_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1229, 597);
+            this.Controls.Add(this.btnAtender);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabBuscar.ResumeLayout(false);
+            this.tabBuscar.PerformLayout();
+            this.tabCadastrar.ResumeLayout(false);
+            this.tabCadastrar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabBuscar;
+        private System.Windows.Forms.TabPage tabCadastrar;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.TextBox txtCPF;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnCarregar;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.TextBox txtFila;
+        private System.Windows.Forms.Label lblAtendimento;
+        private System.Windows.Forms.Button btnAtender;
+    }
+}
+
---
+++ SistemaCadastro/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SistemaCadastro
+{
+    public partial class Form1 : Form
+    {
+        struct tdado
+        {
+            //?
+        };
+        int qtd = 0;
+        
+      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        void mostrar()
+        {
+            
+
+        }
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            
+        }
+        void salvar()
+        {
+            BinaryWriter arq = new BinaryWriter(File.Open("fila.txt", FileMode.Create));
+            arq.Write(fila.Count());
+            foreach (tdado b in fila)
+            {
+                arq.Write(b.nome);
+                arq.Write(b.cpf);
+            }
+            arq.Close();
+            MessageBox.Show("Dados Salvos com Sucesso", "Mensagem");
+        }
+        void carregar()
+        {
+
+            BinaryReader arq = new BinaryReader(
+                File.Open("fila.txt", FileMode.Open));
+            if (File.Exists("fila.txt"))
+            {
+                qtd = arq.ReadInt32();
+                for (int i = 0; i < qtd; i++)
+                {
+                    tdado b;
+                    b.nome = arq.ReadString();
+                    b.cpf = arq.ReadString();
+                    fila.Enqueue(b);                   
+                }// fim for
+                arq.Close();
+                mostrar();
+            }// fim if 
+            else
+                MessageBox.Show("Arquivo não encontrado", "Erro");
+        }
+
+        private void btnCarregar_Click(object sender, EventArgs e)
+        {
+            carregar();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            salvar();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnAtender_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
